3878aa487fca2b7e4aef03dcde70c7c7a683c080
Jerozh <jeronimoh1990@gmail.com>
2025-04-15 10:25:49 -0300

    Create repository
    Creacion del repositorio back end e instalacion de dependencias

---
+++ backend/src/test/java/BackEnd/GestorAlquileres/GestorAlquileresApplicationTests.java
@@ -0,0 +1,13 @@
+package BackEnd.GestorAlquileres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorAlquileresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
