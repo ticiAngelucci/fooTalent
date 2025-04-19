e507acc06ce566a3f638b3eeea9ef3ca341bc907
unknown <jeronimoh1990@gmail.com>
2025-04-19 20:18:46 -0300

    Changes to Swagger's configuration

--- backend/src/main/java/BackEnd/GestorAlquileres/config/SecurityConfig.java
+++ backend/src/main/java/BackEnd/GestorAlquileres/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
--- backend/src/main/java/BackEnd/GestorAlquileres/config/SwaggerConfig.java
+++ backend/src/main/java/BackEnd/GestorAlquileres/config/SwaggerConfig.java
@@ -1,4 +1,28 @@
 package BackEnd.GestorAlquileres.config;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("auth")
+                .pathsToMatch("/api/auth/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API prueba")
+                        .version("1.0")
+                        .description("Documentación de endpoints seleccionados"));
+    }
 }
