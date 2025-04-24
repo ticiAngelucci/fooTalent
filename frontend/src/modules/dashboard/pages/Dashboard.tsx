4d8e21153a294f53d685cc9394274ab71dbffe9f
Tici Angelucci <tittiangelucci08@gmail.com>
2025-04-24 20:11:59 -0300

    add logout and show user name

--- frontend/src/modules/dashboard/pages/Dashboard.tsx
+++ frontend/src/modules/dashboard/pages/Dashboard.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/shared/components/ui/button";
+
 const Dashboard = () => {
-    return (
-      <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", flexDirection: "column",margin:"auto" }}>
-        <h1>Dashboard</h1>
-      </div>
-    );
+  const [username, setUsername] = useState<string | null>(null); 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    if (token) {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const decodedPayload = JSON.parse(atob(base64));
+      console.log(decodedPayload);
+      setUsername(decodedPayload.sub);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    navigate("/login");
   };
-  
-  export default Dashboard;
-  
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen relative">
+      <div className="absolute top-5 left-5 flex items-center gap-2">
+        {username && <p className="text-xl">Hola {username}!</p>}
+      </div>
+      <div className="absolute top-5 right-5">
+        <Button onClick={handleLogout}>
+          Cerrar sesión
+        </Button>
+      </div>
+
+      <h1 className="text-3xl">Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
