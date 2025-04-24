7c090ee2cc6fb062f013cd1a20d8fc3bbe40c7a1
Juane Elizondo <juane.elizondo23@gmail.com>
2025-04-23 21:34:45 -0300

    fix: auth validations fixed

--- frontend/src/modules/auth/schemas/login.schemas.ts
+++ frontend/src/modules/auth/schemas/login.schemas.ts
@@ -1,8 +1,17 @@
 import { z } from "zod";
 
-export const loginSchema = z.object({
-  email: z.string().email("Correo inválido"),
-  password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres").max(35, "La contraseña no puede tener más de 35 caracteres"),
+export const loginSchema = z
+.object({
+  email:
+   z
+   .string()
+   .email("Correo inválido"),
+
+  password:
+   z
+   .string()
+   .min(8, "La contraseña debe tener al menos 6 caracteres")
+   .max(16, "La contraseña no puede tener más de 35 caracteres"),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
