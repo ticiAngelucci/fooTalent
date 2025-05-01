17966add7fdfa6500036784a6b25faaa54c69fec
Tici Angelucci <tittiangelucci08@gmail.com>
2025-05-01 12:51:12 -0300

    dashboard ONLY DESKTOP

--- frontend/src/modules/dashboard/pages/Dashboard.tsx
+++ frontend/src/modules/dashboard/pages/Dashboard.tsx
@@ -1,34 +1,93 @@
-import { useNavigate } from "react-router-dom";
-import { Button } from "@/shared/components/ui/button";
-import { Route } from "@/shared/constants/route";
-
 import { useUserStore } from "@/store/userStore";
 import LogoutButton from "@/shared/components/logoutButton/LogoutButton";
+import SummaryCard from "@/shared/components/summaryCard/SummaryCard";
+import InfoCard from "@/shared/components/infoCard/InfoCard";
 
+const summary = [
+  { label: "Pagos vencidos", value: 4, icon: "\u26A0\uFE0F" },
+  { label: "Pagos pendientes", value: 6, icon: "\u2139\uFE0F" },
+  { label: "Pagos al día", value: 12, icon: "\u2714\uFE0F" },
+];
+
+const properties = [
+  {
+    type: "Casa",
+    location: "San Andrés #4613",
+    status: "Disponible",
+    image: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FzYSUyMG1vZGVybmF8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    type: "Depto",
+    location: "San Andrés #4613",
+    status: "Disponible",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROM_tkr50bz0XK-3ZPQKiyW7gNj6HLuyhWrw&s",
+  },
+];
+
+const contracts = [
+  { name: "Maria García", location: "San Andrés #4613" },
+  { name: "Juan Perez", location: "San Andrés #4613" },
+  { name: "José Rodríguez", location: "San Ignacio #4614" },
+];
+
+const contacts = [
+  { type: "Casa", location: "San Andrés #4613" },
+  { type: "Casa", location: "San Andrés #4613" },
+  { type: "Casa", location: "San Andrés #4613" },
+];
+const infoSections = [
+  {
+    type: "property",
+    title: "Inmuebles",
+    subtitle:"Listado de inmuebles",
+    items: properties,
+  },
+  {
+    type: "contract",
+    title: "Contratos",
+    subtitle:"Contratos vigentes",
+    items: contracts,
+  },
+  {
+    type: "contact",
+    title: "Contactos",
+    subtitle:"Listado de contratos",
+    items: contacts,
+  },
+];
 const Dashboard = () => {
-  const navigate = useNavigate();
   const username = useUserStore((state) => state.username);
 
-  if(!username) return null;
-
-  const handleButtonClick = () => {    
-    navigate(Route.GetAllUsers);
-  };
+  if (!username) return null;
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen relative">
-      <div className="absolute top-5 left-5 flex items-center gap-2">
-        {username && <p className="text-xl">Hola {username}!</p>}
+    <div className="flex flex-col justify-center items-center ">
+      <div className="flex flex-row justify-end items-center w-[95%] mx-auto p-4">
+        <LogoutButton />
       </div>
-      <div className="absolute top-5 right-5">
-        <LogoutButton/>
-      </div>
+      <div className="min-h-screen flex flex-col w-[90%] p-6 space-y-6">
+  <h1 className="text-2xl font-thin">¡Bienvenido, {username}!</h1>
+  <h1 className="text-2xl font-thin">Gestiona tus alquileres de forma fácil y eficiente</h1>
 
-      <h1 className="text-3xl">Dashboard</h1>
-      
-        <Button onClick={handleButtonClick}>
-          Ver todos los usuarios
-        </Button>
+  <div className="grid grid-cols-1 sm:grid-cols-3 justify-items-center mt-10"> 
+    {summary.map((item, idx) => (
+      <SummaryCard key={idx} {...item} />
+    ))}
+  </div>
+
+  <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-10">
+    {infoSections.map((section, idx) => (
+      <InfoCard
+        key={idx}
+        type={section.type}
+        subtitle={section.subtitle}
+        title={section.title}
+        items={section.items}
+      />
+    ))}
+  </div>
+</div>
+
     </div>
   );
 };
--- frontend/src/shared/components/logoutButton/LogoutButton.tsx
+++ frontend/src/shared/components/logoutButton/LogoutButton.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useUserStore } from "@/store/userStore";
 import { Button } from "@/shared/components/ui/button";
 import { Route } from "@/shared/constants/route";
+import { LogOut } from "lucide-react";
 
 const LogoutButton = () => {
   const logout = useUserStore((state) => state.logout);
@@ -13,9 +14,16 @@ const LogoutButton = () => {
   };
 
   return (
-    <Button onClick={handleLogout} variant="outline">
-      Cerrar sesión
-    </Button>
+    <div className="flex flex-col items-center space-y-1">
+      <Button
+        onClick={handleLogout}
+        variant="ghost"
+        className="bg-[#6C6B75] hover:bg-[#5c5b64] w-10 h-10 rounded-xl p-2"
+      >
+        <LogOut className="text-white w-5 h-5" />
+      </Button>
+      <span className="text-xs text-muted-foreground">Logout</span>
+    </div>
   );
 };
 
