697ad029577269a34785a7bab96b3bc9a728f96e
Mateo Viera <mateogini10@gmail.com>
2025-05-03 14:04:43 -0300

    feat:add sidebar

--- frontend/src/modules/dashboard/pages/Dashboard.tsx
+++ frontend/src/modules/dashboard/pages/Dashboard.tsx
@@ -4,32 +4,39 @@ import { Route } from "@/shared/constants/route";
 
 import { useUserStore } from "@/store/userStore";
 import LogoutButton from "@/shared/components/logoutButton/LogoutButton";
+import { SidebarProvider } from "@/shared/components/ui/sidebar";
+import { AppSidebar } from "@/shared/components/sidebar/app-sidebar";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const username = useUserStore((state) => state.username);
 
-  if(!username) return null;
+  if (!username) return null;
 
-  const handleButtonClick = () => {    
+  const handleButtonClick = () => {
     navigate(Route.GetAllUsers);
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen relative">
-      <div className="absolute top-5 left-5 flex items-center gap-2">
-        {username && <p className="text-xl">Hola {username}!</p>}
+    <>
+      <div className="fixed left-0 top-0 h-full z-50">
+        <SidebarProvider>
+          <AppSidebar />
+        </SidebarProvider>
       </div>
-      <div className="absolute top-5 right-5">
-        <LogoutButton/>
-      </div>
+      <div className="flex flex-col justify-center items-center h-screen relative">
+        <div className="absolute top-5 left-5 flex items-center gap-2">
+          {username && <p className="text-xl">Hola {username}!</p>}
+        </div>
+        <div className="absolute top-5 right-5">
+          <LogoutButton />
+        </div>
 
-      <h1 className="text-3xl">Dashboard</h1>
-      
-        <Button onClick={handleButtonClick}>
-          Ver todos los usuarios
-        </Button>
-    </div>
+        <h1 className="text-3xl">Dashboard</h1>
+
+        <Button onClick={handleButtonClick}>Ver todos los usuarios</Button>
+      </div>
+    </>
   );
 };
 
