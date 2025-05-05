dc4e495cf997321d64a86c6b3ac4e7e6837d5be1
Juane Elizondo <juane.elizondo23@gmail.com>
2025-05-05 19:21:13 -0300

    auth redesign ready

--- frontend/src/modules/auth/pages/Login.tsx
+++ frontend/src/modules/auth/pages/Login.tsx
@@ -1,19 +1,18 @@
 import { GoogleLoginButton } from "../components/GoogleLoginButton";
 import LoginForm from "../components/LoginForm";
 import edificio1 from "../assets/edificio1.png";
-import Logo from "../assets/Logo.svg";
 
 const Login = () => {
   return (
     <div className="flex">
       <section className="hidden lg:block lg:w-2/3 xl:w-1/2 h-screen bg-brand-200 relative">
-        <img src={Logo} alt="" className="absolute top-10 left-10"/>
+        <img src="/Logo.svg" alt="" className="absolute top-10 left-10"/>
         <p className="mt-42 text-4xl font-semibold px-10"><span className="text-brand-800">Gestiona</span> tus alquileres de forma <span className="text-brand-800">fácil</span> y <span className="text-brand-800">eficiente</span></p>
         <img src={edificio1} alt="" className="w-4xl absolute bottom-0"/>
       </section>
       <section className="w-full h-screen flex flex-col items-center lg:justify-center bg-gradient-to-br from-gray-100 via-white to-gray-200">
         <div className="w-full flex justify-around items-center">
-          <img src={Logo} alt="Logo Rentary" className="lg:hidden size-38"/>
+          <img src="/Logo.svg" alt="Logo Rentary" className="lg:hidden size-38"/>
           <a href="/register" className=" text-[#1E40AF] underline lg:absolute lg:top-10 lg:right-20">
             Registrarse
           </a>
--- frontend/src/modules/auth/pages/Register.tsx
+++ frontend/src/modules/auth/pages/Register.tsx
@@ -1,6 +1,5 @@
 import { GoogleLoginButton } from "../components/GoogleLoginButton";
 import RegisterForm from "../components/RegisterForm";
-import Logo from "../assets/Logo.svg";
 import edificio2 from "../assets/edificio2.png";
 
 const Register = () => {
@@ -9,13 +8,13 @@ const Register = () => {
   return (
     <div className="flex">
       <section className="hidden lg:block lg:w-2/3 xl:w-1/2 h-screen bg-brand-200 relative">
-        <img src={Logo} alt="" className="absolute top-10 left-10"/>
+        <img src="/Logo.svg" alt="" className="absolute top-10 left-10"/>
         <p className="mt-42 text-4xl font-semibold px-10"><span className="text-brand-800">Gestiona</span> tus alquileres de forma <span className="text-brand-800">fácil</span> y <span className="text-brand-800">eficiente</span></p>
         <img src={edificio2} alt="Logo Rentary" className="w-4xl absolute bottom-0"/>
       </section>
       <section className="w-full h-screen flex flex-col items-center lg:justify-center bg-gradient-to-br from-gray-100 via-white to-gray-200">
         <div className="w-full flex justify-around items-center">
-          <img src={Logo} alt="Logo Rentary" className="lg:hidden size-38"/>
+          <img src="/Logo.svg" alt="Logo Rentary" className="lg:hidden size-38"/>
           <a href="/login" className=" text-[#1E40AF] underline lg:absolute lg:top-10 lg:right-20">
             Iniciar sesión
           </a>
