b736fbd8c37c283374064611137b806e43cac520
Lucascabj4710 <lucascabj2605@gmail.com>
2025-05-11 19:45:26 -0300

    creation of search bar

--- backend/src/main/java/BackEnd/Rentary/Tenants/repositories/TenantsRepository.java
+++ backend/src/main/java/BackEnd/Rentary/Tenants/repositories/TenantsRepository.java
@@ -5,6 +5,11 @@ import BackEnd.Rentary.Tenants.entities.Tenants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TenantsRepository extends JpaRepository<Tenants, Long> {
+
+    List<Tenants> findByNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrDniContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String lastName, String dni, String email);
+
 }
