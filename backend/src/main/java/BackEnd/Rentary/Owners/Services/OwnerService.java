b37de825d684a00e654ba0b90753e6a2580eea56
Victor Molinas <u.vmolinas@gmail.com>
2025-05-15 14:03:52 -0300

    add endpoint for get propertyByIdOwner | safe delete fon contracts and tenant/owners

--- backend/src/main/java/BackEnd/Rentary/Owners/Services/OwnerService.java
+++ backend/src/main/java/BackEnd/Rentary/Owners/Services/OwnerService.java
@@ -2,9 +2,13 @@ package BackEnd.Rentary.Owners.Services;
 
 import BackEnd.Rentary.Owners.DTOs.OwnerRequestDto;
 import BackEnd.Rentary.Owners.DTOs.OwnerResponseDto;
+import BackEnd.Rentary.Propertys.DTOs.PropertyResponseDto;
+import BackEnd.Rentary.Propertys.Entities.Property;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface OwnerService {
     ResponseEntity<?> getOwnerId(Long id);
@@ -12,4 +16,5 @@ public interface OwnerService {
     ResponseEntity<?> deleteOwner(Long id);
     ResponseEntity<?> updateOwner(Long id, OwnerRequestDto ownerDto);
     Page<OwnerResponseDto> getOwner(Pageable pageable);
+    List<PropertyResponseDto> getPropertiesByOwnerId(Long id);
 }
