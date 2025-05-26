1f31429be5797ab0c4717f0ca5aa2e9a8be80b91
Alexander Riascos <alexriascos159@gmail.com>
2025-05-26 08:05:38 -0500

    Add fix payments envs vars

--- frontend/src/modules/payments/servises/paymentService.ts
+++ frontend/src/modules/payments/servises/paymentService.ts
@@ -1,13 +1,14 @@
 import axios from "axios"
 import { getAuthToken } from "../store/authStore"
 import { ApiResponse } from "../types/pyments"
+import { API_URL } from "@/shared/constants/api";
 
 
 export const fetchPayments = async (currentPage: number, pageSize: number): Promise<ApiResponse> => {
   try {
     const token = getAuthToken()
     const response = await axios.get<ApiResponse>(
-      `https://rrentary.koyeb.app/payments/all-details?page=${currentPage}&size=${pageSize}`,
+      `${API_URL}/payments/all-details?page=${currentPage}&size=${pageSize}`,
       {
         headers: {
           Authorization: `Bearer ${token}`
