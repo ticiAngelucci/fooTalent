f3840b951256e7b3aeb04643e4606223c0fb711e
Juane Elizondo <juane.elizondo23@gmail.com>
2025-05-29 18:58:40 -0300

    login error fixed and some improvements at landing page

--- frontend/src/modules/auth/pages/Login.tsx
+++ frontend/src/modules/auth/pages/Login.tsx
@@ -3,15 +3,15 @@ import LoginForm from "../components/LoginForm";
 import loginFrame from "../assets/iniciar.webp";
 import { Link } from "react-router-dom";
 import { Route } from "@/shared/constants/route";
-import { Button } from "react-day-picker";
-import { ArrowLeft } from "lucide-react";
 
 
 const Login = () => {
   return (
     <div className="flex min-h-screen">
       <section className="hidden lg:block lg:w-full min-h-screen bg-brand-200 text-center relative overflow-hidden">
-        <img src="/Logo.svg" alt="" className="absolute top-10 left-10" />
+        <Link to={Route.Home}>
+          <img src="/Logo.svg" alt="" className="absolute top-10 left-10" />
+        </Link>
         <p className="mt-42 text-5xl font-bold text-start px-10 mx-auto relative z-50 justify-start">
           <span className="text-brand-800">Gestiona</span> tus alquileres de
           forma <span className="text-brand-800">fácil</span> y{" "}
@@ -25,13 +25,9 @@ const Login = () => {
       </section>
       <section className="w-full flex flex-col items-center lg:justify-center bg-gradient-to-br from-gray-100 via-white to-gray-200 lg:pt-32 lg:pb-56 min-h-screen">
         <div className="w-full flex justify-around items-center">
-          <img src="/Logo.svg" alt="Logo Rentary" className="lg:hidden size-38"/>
-          <a href="/">
-            <Button  className="gap-2 bg-white !underline hover:bg-blue lg:top-10 text-gray box-shadow-none">
-              <ArrowLeft className="w-4 h-4" />
-              Volver al inicio
-            </Button>
-          </a>
+          <Link to={Route.Home}>
+            <img src="/Logo.svg" alt="Logo Rentary" className="lg:hidden size-38"/>
+          </Link>
           <a href="/register" className=" text-[#1E40AF] !underline lg:absolute lg:top-10 lg:right-20 ">
             Registrarse
           </a>
